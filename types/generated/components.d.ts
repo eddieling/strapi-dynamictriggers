49061af5a7900989f1f4e35b0b0a5d2df9b7787c
eddieling <eddielingg@gmail.com>
2025-04-02 08:43:54 +0800

    Add displays

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,36 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface DisplayDisplays extends Schema.Component {
+  collectionName: 'components_display_displays';
+  info: {
+    displayName: 'Displays';
+    icon: 'slideshow';
+  };
+  attributes: {
+    Displays: Attribute.Component<'display.resolution', true>;
+  };
+}
+
+export interface DisplayResolution extends Schema.Component {
+  collectionName: 'components_display_resolutions';
+  info: {
+    displayName: 'Resolution';
+    icon: 'slideshow';
+  };
+  attributes: {
+    Resolutions: Attribute.Enumeration<
+      ['w800h1140', 'w1024h768', 'w1200h1710', 'w1600h980']
+    >;
+    Image: Attribute.Media;
+    Video: Attribute.Media;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'display.displays': DisplayDisplays;
+      'display.resolution': DisplayResolution;
+    }
+  }
 }
